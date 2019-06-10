cf6ad07e1af97d60928b76862a4e1e2950864db8
tongyibin <1078170466@qq.com>
2019-06-10 22:17:01 +0800

    登录

--- src/router.js
+++ src/router.js
@@ -3,7 +3,7 @@ import Router from 'vue-router'
 
 Vue.use(Router)
 
-export default new Router({
+const router = new Router({
   routes: [
     {
       path: '/',
@@ -12,7 +12,7 @@ export default new Router({
         {
           path: 'film',
           name: 'film',
-          component: () => import('./views/index/film')
+          component: () => import('./views/index/film.vue')
         },
         {
           path: 'cinema',
@@ -22,18 +22,49 @@ export default new Router({
         {
           path: 'center',
           name: 'center',
-          component: () => import('./views/index/center.vue')
+          component: () => import('./views/index/center.vue'),
+          meta:{
+            requireLogin: true
+          }
         },
         {
           path: '',
           redirect: '/film'
-        }
+        },
+
+
       ]
     },
     {
       path: '/city',
       name: 'city',
       component: () => import('./views/city/index.vue')
-    }
+    },
+    {
+      path:'/login',
+      name:'login',
+      component:()=>import('./views/login/index.vue')
+    },
   ]
 })
+// 全局前置守卫
+router.beforeEach((to,from,next)=>{
+  if(to.meta.requireLogin){
+    //判断是否有登录
+    if(window.localStorage.getItem('userInfo')){
+      next()
+    }else{
+      next({
+        path:'/login',
+        query:{
+          redirect:to.fullPath
+        }
+      })
+    }
+
+  }
+  // console.log(to)
+  next()
+})
+
+export default router
---
+++ src/views/login/index.vue
@@ -0,0 +1,26 @@
+<template>
+  <div>
+    <h1>登录页面</h1>
+    <input type="text" placeholder="账号">
+    <input type="password" placeholder="密码">
+    <button @click="login">login</button>
+  </div>
+</template>
+<script>
+import { setTimeout } from 'timers';
+export default {
+  methods:{
+    login(){
+      setTimeout(()=>{
+        let userInfo={
+          name:'张三',
+          age:18
+        }
+        window.localStorage.setItem('userInfo',JSON.stringify(userInfo))
+        let redirect = this.$route.query.redirect || '/film'
+        this.$router.replace(redirect)
+      },2000)
+    }
+  }
+}
+</script>
